9d511ef9b1a85957e5d536994e4c04d45df86c78
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2020-12-20 22:38:38 +0200

    wip databasing

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 // Routes
 var index = require('./routes/index');
+var setup = require('./routes/setup');
+
 app.use('/', index);
+app.use('/', setup);
 
 //View Engine
 app.set('view engine', 'ejs');
--- config/db.js
+++ config/db.js
@@ -1,17 +1,10 @@
-const { Client } = require('node-postgres');
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+  port: 5432,
+});
 
-(async () => {
-  const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port: 5432,
-  });
-
-  await client.connect();
-
-  const res = await client.query('SELECT * from users');
-  console.log(res);
-  await client.end();
-})().catch(console.error);
+module.exports = pool;
