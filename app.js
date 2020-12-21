04f5f8356a45584e7dc7129e7ced2081b6631b1d
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2020-12-21 23:08:18 +0200

    add post form

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
+
 // Routes
 var index = require('./routes/index');
-var setup = require('./routes/setup');
 
 app.use('/', index);
-app.use('/', setup);
+app.use('/posts', index);
 
 //View Engine
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../config/db');
 
 // // middleware that is specific to this router
 // router.use(function timeLog(req, res, next) {
@@ -7,8 +8,36 @@ var router = express.Router();
 //   next();
 // });
 
-router.get('/', (req, res) => {
-  res.render('../views/index');
+router.get('/setup', async (req, res) => {
+  const q = `insert into posts (title, content, user_id ) values ($1, $2,$3) returning *`;
+  const v = [`Hello World`, `lorem ipsum vola content`, 1];
+  const results = await pool
+    .query(q, v)
+    .then(res => console.log(res.rows[0]))
+    .catch(err => console.log(err));
+
+  res.send(results);
+});
+
+router.get(`/`, async (req, res) => {
+  const query = `SELECT * FROM posts`;
+
+  await pool
+    .query(query)
+    .then(resp =>
+      res.render(`../views/index`, {
+        posts: resp.rows,
+      })
+    )
+    .catch(err => res.send(`database error`));
+});
+
+router.get(`/add`, (req, res) => {
+  res.render(`add-post`, {});
+});
+
+router.post(`/`, (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
