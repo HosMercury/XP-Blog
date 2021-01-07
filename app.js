af6ea54035bae0a7a8808761bf4288410908a24c
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2021-01-07 14:17:49 +0200

    auth

--- app.js
+++ app.js
@@ -1,41 +1,21 @@
 require('dotenv').config();
 const express = require('express');
-const redis = require('redis');
-const session = require('express-session');
-const redisStore = require('connect-redis')(session);
-const client = redis.createClient();
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const mongoose = require('morgan');
+const cookieParser = require('cookie-parser');
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(require('morgan')('dev'));
 app.use(express.static(__dirname + '/public'));
-
-//session
-app.use(
-  session({
-    secret: process.env.SESSION_KEY,
-    // create new redis store.
-    store: new redisStore({
-      host: 'localhost',
-      port: 6379,
-      client: client,
-      ttl: 260,
-    }),
-    saveUninitialized: false,
-    resave: false,
-  })
-);
 
 // Routes
 var postsRouter = require('./routes/postsRouter');
-var authRouter = require('./routes/authRouter');
+var usersRouter = require('./routes/usersRouter');
 
 app.use('/posts', postsRouter);
-app.use('/', authRouter);
+app.use('/users', usersRouter);
 app.get('/', (req, res) => res.status(304).redirect('/posts?p=1'));
 
 // app.get('/', (req, res) => req.render('posts/list'));
